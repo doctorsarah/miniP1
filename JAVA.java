bf2e80a686f54bf0c91892726382c5cb1859b99f
Dr. Sarah <32044936+doctorsarah@users.noreply.github.com>
2021-01-09 14:47:34 -0500

    Update JAVA.java

--- JAVA.java
+++ JAVA.java
@@ -1,11 +1,10 @@
-//****************************************************************************************
-// NAME: <replace with your name, first and last
-// DATE: <replace with the date you started working on this program
-// HOURS: <replace with number of hours, or fraction thereof, you worked on this program
-// CLASS: <course ID goes here>
-// ASSSIGNMENT: <program name goes here>
-//****************************************************************************************
-
+/**
+ * @author: replace with your name, first and last
+ * DATE: replace with the date you started working on this program
+ * HOURS: <replace with number of hours, or fraction thereof, you worked on this program
+ * CLASS: <course ID goes here>
+ * ASSIGNMENT: <program name goes here>
+ */
 public class JAVA {
   public static void main (String[] args) {
     // put your code to produce the correct output here
